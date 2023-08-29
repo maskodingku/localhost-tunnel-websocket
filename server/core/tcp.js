4f18b4afadeef149efc949a8ba205a9cadf9f251
Abdi Syahputra Harahap <62774779+maskodingku@users.noreply.github.com>
2023-08-29 12:08:54 +0700

    Create tcp.js

---
+++ server/core/tcp.js
@@ -0,0 +1,11 @@
+import net from "net";
+export default {
+	"init" : async (port,server_tunnel)=>{
+		return new Promise((resolve)=>{
+			const server = net.createServer().listen(port, () => {
+			  console.log('Server TCP listening on port :',port);
+			  resolve(server);
+			});
+		});
+	}
+};
