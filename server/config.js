5eeb8083a4145dbe95cc4db7afd9dcbd093253c4
Abdi Syahputra Harahap <62774779+maskodingku@users.noreply.github.com>
2023-08-29 12:05:12 +0700

    Create config.js

---
+++ server/config.js
@@ -0,0 +1,12 @@
+
+export default ({
+	"port-tunnel" : 80,
+	"port-tcp":8081,
+	"tcp-traffic-encryption":{
+		"status":true,
+		"key":"pantek69"
+	},
+	"tunnel-authentication":{
+		"token":"asedekontil66"
+	}
+});
