0b07dbaeae89919a67da13695e2dcdaf87810145
Abdi Syahputra Harahap <62774779+maskodingku@users.noreply.github.com>
2023-08-29 12:45:36 +0700

    Create config.js

---
+++ client/config.js
@@ -0,0 +1,10 @@
+export default ({
+	"host-tunnel" : "123.123.123.123",
+	"port-tunnel" : 80,
+	"host-tcp" : "localhost",
+	"port-tcp" : 80,
+	"tcp-traffic-encryption-key":"pantek69",
+	"tunnel-authentication":{
+		"token":"asedekontil66"
+	}
+});
