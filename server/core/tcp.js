7cd38b410e993486b696a38109d39a8cf5a0f965
Abdi Syahputra Harahap <62774779+maskodingku@users.noreply.github.com>
2023-08-30 11:21:24 +0700

    Update tcp.js

--- server/core/tcp.js
+++ server/core/tcp.js
@@ -3,8 +3,8 @@ export default {
 	"init" : async (port,server_tunnel)=>{
 		return new Promise((resolve)=>{
 			const server = net.createServer().listen(port, () => {
-			  console.log('Server TCP listening on port :',port);
-			  resolve(server);
+			  	console.log('Server TCP listening on port :',port);
+			  	resolve(server);
 			});
 		});
 	}
