b50ca3e31612c8d5bd12c28508255820d889a06a
Abdi Syahputra Harahap <62774779+maskodingku@users.noreply.github.com>
2023-08-30 11:24:54 +0700

    Update tunneling.js

--- server/core/tunneling.js
+++ server/core/tunneling.js
@@ -1,10 +1,7 @@
 import tcpId from "./tcp-id.js";
 import encrypt from "./encrypt.js";
 import decrypt from "./decrypt.js";
-
-
 export default (tcp,tunnel,config)=>{
-
 	let client_socket = null;
 	tcp.on('connection', (clientTcp) => {
 		console.log('New TCP Connected...');
@@ -16,75 +13,68 @@ export default (tcp,tunnel,config)=>{
 			clientTcp.end();
 			clientTcp.destroy();
 		};
-	  clientTcp.on('data', (data) => {
-	    if(client_socket!=null){
-	    	// Enkripsi data sebelum mengirimkannya.
-			  const dataEncrypt = encrypt(data,config["tcp-traffic-encryption"].key);
-			  client_socket.emit("tcp-server-data",{
-			  	"id":id,
-			  	"buff":dataEncrypt
-			  });
+	  	clientTcp.on('data', (data) => {
+	    		if(client_socket!=null){
+	    			// Enkripsi data sebelum mengirimkannya.
+			  	const dataEncrypt = encrypt(data,config["tcp-traffic-encryption"].key);
+			  	client_socket.emit("tcp-server-data",{
+			  		"id":id,
+			  		"buff":dataEncrypt
+			  	});
 			};
-	  }).on('end', () => {
-	    console.log('Koneksi Tcp klien ditutup');
-	    if(client_socket!=null){
-			  client_socket.emit("tcp-server-end",id);
-			  tcpId.remove(id);
-			  // clientTcp.end();
-			  clientTcp.destroy();
+	  	}).on('end', () => {
+	    		console.log('Koneksi Tcp klien ditutup');
+	    		if(client_socket!=null){
+				client_socket.emit("tcp-server-end",id);
+				tcpId.remove(id);
+				// clientTcp.end();
+				clientTcp.destroy();
 			}else{
 				// clientTcp.end();
 				clientTcp.destroy();
 			};
-	  }).on('close', () => {
-		  console.log('Koneksi Tcp client telah ditutup sepenuhnya.');
-		  if(client_socket!=null){
-			  client_socket.emit("tcp-server-close",id);
-			  tcpId.remove(id);
-			  // clientTcp.end();
-			  clientTcp.destroy();
+	  	}).on('close', () => {
+			console.log('Koneksi Tcp client telah ditutup sepenuhnya.');
+			if(client_socket!=null){
+				client_socket.emit("tcp-server-close",id);
+				tcpId.remove(id);
+				// clientTcp.end();
+				clientTcp.destroy();
 			}else{
 				// clientTcp.end();
 				clientTcp.destroy();
 			};
 		}).on('error', (err) => {
-	    console.error('Kesalahan pada koneksi client:', err);
-	    if(client_socket!=null){
-			  client_socket.emit("tcp-server-error",id,err);
-			  // clientTcp.end();
-			  clientTcp.destroy();
+	    		console.error('Kesalahan pada koneksi client:', err);
+	    		if(client_socket!=null){
+				client_socket.emit("tcp-server-error",id,err);
+				// clientTcp.end();
+				clientTcp.destroy();
 			}else{
 				// clientTcp.end();
 				clientTcp.destroy();
 			};
-	  });	
+	  	});	
 	});
-
-
-
-
-
 	tunnel.on('connection', (socket) => {
-	  console.log('a user connected');
-	  client_socket = socket;
-	  socket.on("tcp-client-data",(id,data)=>{
-	  	// Mendekripsi data yang diterima.
-	  	const dataDecrypt = decrypt(data,config["tcp-traffic-encryption"].key);
+	  	console.log('a user connected');
+	  	client_socket = socket;
+	  	socket.on("tcp-client-data",(id,data)=>{
+	  		// Mendekripsi data yang diterima.
+	  		const dataDecrypt = decrypt(data,config["tcp-traffic-encryption"].key);
 			tcpId.sendData(id,dataDecrypt);
 		});
 		socket.on("tcp-client-end",(id)=>{
 			console.log("tcp-client-end",id);
 			tcpId.sendEnd(id);
 		});
-	  socket.on("tcp-client-error",(id,err)=>{
+	  	socket.on("tcp-client-error",(id,err)=>{
 			console.log("tcp-client-error",id,err);
 			tcpId.sendError(id);
 		});
-	  socket.on('disconnect', () => {
-	    console.log('user disconnected');
-	    client_socket = null;
-	  });
+	  	socket.on('disconnect', () => {
+	    		console.log('user disconnected');
+	    		client_socket = null;
+	  	});
 	});
-
-
 };
